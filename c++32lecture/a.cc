33b34e89e4a0f8970e840578683d615c24747a87
kikyou <574479389@qq.com>
2022-12-20 23:14:04 +0800

    c++32 lecture

---
+++ c++32lecture/a.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+
+/*
+
+RAII(资源的获得即初始化) : 资源获取后立即放进管理对象; 管理对象通过析构函数保证资源被释放
+
+这个机制利用栈和析构函数来管理内存(包括堆内存)
+
+其原理是:函数返回后，移动栈顶指针释放掉栈帧, 此时编译器会自动对本地变量调用其析构函数。
+即便是返回前程序触发了异常, 仍会调用析构函数。
+
+利用RAII机制, 可以实现智能指针来包裹起裸指针,智能指针是栈上的本地变量, 从而安全使用
+下面是一个RAII实现简易版智能指针的例子
+
+*/
+
+
+enum class shape_type {
+    circle,
+    triangle,
+};
+
+// 偷点懒省略了虚析构
+class shape {};
+class circle : public shape  {};
+class triangle : public shape {};
+
+shape* create_shape(shape_type type) 
+{
+    if (type == shape_type::circle) {
+        return new circle();
+    }
+    else {
+        return new triangle();
+    }
+};
+
+class shape_wrapper {
+public:
+    explicit shape_wrapper(shape* ptr = nullptr) 
+        : ptr_(ptr) {}
+    ~shape_wrapper() {
+        delete ptr_;
+    }
+private:
+    shape* ptr_;
+};
+
+void foo() 
+{
+    // ptr_wrap是本地变量, foo返回时编译器会自动调用其析构函数
+    // 从而安全使用create_shape()返回的裸指针
+    shape_wrapper ptr_wrap(create_shape(shape_type::circle));
+}
+
+int main() 
+{
+    std::cout << "hello, modern c++" << std::endl;
+    return 0; 
+}
